37f28d0d78085f8e606b8c62f70043853d592041
Husky3388 <jasont3388@gmail.com>
2016-11-15 15:37:49 -0800

    new calendar?

--- app/src/main/java/com/example/jasonthai/project/ui/CalendarActivity.java
+++ app/src/main/java/com/example/jasonthai/project/ui/CalendarActivity.java
@@ -2,6 +2,9 @@ package com.example.jasonthai.project.ui;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
+import android.provider.CalendarContract;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +16,7 @@ import com.example.jasonthai.project.R;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    CalendarView calendar;
+    /*CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,42 @@ public class CalendarActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), (month+1) + "/" + dayOfMonth + "/" + year, Toast.LENGTH_LONG).show();
             }
         });
-    }
+    }*/
+    Calendar cal = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        /*Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra("beginTime", cal.getTimeInMillis());
+        intent.putExtra("allDay", false);
+        intent.putExtra("rrule", "FREQ=DAILY");
+        intent.putExtra("endTime", cal.getTimeInMillis() + 60 * 60 * 1000);
+        intent.putExtra("title", "A Test Event from android app");
+        startActivity(intent);*/
+
+        Intent calIntent = new Intent(Intent.ACTION_INSERT);
+        calIntent.setType("vnd.android.cursor.item/event");
+        calIntent.putExtra(CalendarContract.Events.TITLE, "My House Party");
+        calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My Beach House");
+        calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "A Pig Roast on the Beach");
+
+        GregorianCalendar calDate = new GregorianCalendar(2016, 11, 21);
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                calDate.getTimeInMillis());
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                calDate.getTimeInMillis());
+
+        startActivity(calIntent);
+    };
+
+
 
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()) {
